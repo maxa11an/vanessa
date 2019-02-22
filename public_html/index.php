97c2810d2f61a8ba0f37e7133015e043f333cfda
Max Allan Niklasson <hello@maxallan.se>
2019-02-23 00:20:59 +0100

    Tidying index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-session_start();
-
 /**
  * Created by PhpStorm.
  * User: maxallan
@@ -11,6 +6,23 @@ session_start();
  * Time: 21:28
  */
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+session_start();
+
+
+
+if (PHP_SAPI == 'cli-server') {
+	$_SERVER['SCRIPT_NAME'] = basename(__FILE__);
+	// To help the built-in PHP dev server, check if the request was actually for
+	// something which should probably be served as a static file
+	$url  = parse_url($_SERVER['REQUEST_URI']);
+	$file = __DIR__ . $url['path'];
+	if (is_file($file)) {
+		return false;
+	}
+}
 require_once __DIR__."/../vendor/autoload.php";
 
 
