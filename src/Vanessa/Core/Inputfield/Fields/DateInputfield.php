4a2625d255e24def880e54b16a952f1ca81b7e36
Max Allan Niklasson <hello@maxallan.se>
2019-02-26 01:10:47 +0100

    Update

---
+++ src/Vanessa/Core/Inputfield/Fields/DateInputfield.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maxallan
+ * Date: 2019-02-25
+ * Time: 21:44
+ */
+
+namespace Vanessa\Core\Inputfield\Fields;
+
+
+use Vanessa\Core\Inputfield\Inputfield;
+use Vanessa\Core\Inputfield\InputfieldInterface;
+use Vanessa\Core\Localization;
+
+class DateInputfield extends Inputfield implements InputfieldInterface
+{
+	const NAME = "Text";
+	public static function renderAdd()
+	{
+		$p = parent::renderAdd();
+		$t = [
+			"title" => Localization::__("Date Field"),
+			"icon" => "https://via.placeholder.com/100?text=Icon",
+			"sort" => 6
+		];
+
+		return array_merge($p, $t);
+	}
+
+
+
+	public function renderField()
+	{
+
+	}
+
+	public function renderOptions()
+	{
+		// TODO: Implement renderOptions() method.
+	}
+}
---
+++ src/Vanessa/Core/Inputfield/Fields/RadiogroupInputfield.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maxallan
+ * Date: 2019-02-25
+ * Time: 22:06
+ */
+
+namespace Vanessa\Core\Inputfield\Fields;
+
+use Vanessa\Core\Inputfield\Inputfield;
+use Vanessa\Core\Inputfield\InputfieldInterface;
+use Vanessa\Core\Localization;
+
+class RadiogroupInputfield extends Inputfield implements InputfieldInterface
+{
+	const NAME = "Radiogroup";
+	public static function renderAdd()
+	{
+		$p = parent::renderAdd();
+		$t = [
+			"title" => Localization::__("Radiogroup"),
+			"icon" => "https://via.placeholder.com/100?text=Icon",
+			"sort" => 5
+		];
+		return array_merge($p, $t);
+	}
+
+
+
+	public function renderField()
+	{
+		// TODO: Implement renderField() method.
+	}
+
+	public function renderOptions()
+	{
+		// TODO: Implement renderOptions() method.
+	}
+}
